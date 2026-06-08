f74b71e67beb14d2344b5717f925f2e96a4d336a
Rajdeep <rajdeepsawant919@gmail.com>
2026-06-09 00:05:04 +0530

    first commit

---
+++ Modules/packages/maths.py
@@ -0,0 +1,4 @@
+def add(a,b):
+    return a+b
+def subs(a,b):
+    return a-b
---
+++ Modules/test.py
@@ -0,0 +1,5 @@
+from packages.maths import *
+from packages.subpack.mult import *
+print(add(12,14))
+print(subs(50,29))
+print(multi(4,9))
